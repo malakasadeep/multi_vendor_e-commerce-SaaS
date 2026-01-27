6d9328711ea5c9e9bb60b495b8d2b88890dbcc67
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2026-01-28 03:37:20 +0530

    Enhance API Gateway and Auth Service with middleware and health check endpoints

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,13 +6,46 @@
 import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    allowedHeaders:["Authorization", "Content-Type"],
+    credentials: true,
+  }
+));
 
-app.get('/api', (req, res) => {
+app.use(morgan('dev'));
+app.use(express.json({limit: "100mb"}));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req:any) => (req.user ? 1000 : 100), // limit each IP to 100 requests per windowMs for unauthenticated users, 1000 for authenticated
+  message: { error: 'Too many requests, please try again later.' },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req:any) => req.ip,
+});
+
+app.use(limiter);
+
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+
+app.use("/", createProxyMiddleware({ target: "http://localhost:6001", changeOrigin: true })); // Auth Service
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -5,17 +5,29 @@
 
 import express from 'express';
 import * as path from 'path';
+import  cors from 'cors';
 
 const app = express();
 
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    allowedHeaders:["Authorization", "Content-Type"],
+    credentials: true,
+  }
+));
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to auth-service!' });
 });
 
 const port = process.env.PORT || 6001;
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Auth service listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on('error', (err) => {
+  console.log("Server error: ", err);
+});
