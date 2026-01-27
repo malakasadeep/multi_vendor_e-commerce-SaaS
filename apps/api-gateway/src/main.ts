810b85437521e02496adf257912857bd3fa8f930
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2026-01-28 02:58:18 +0530

    first commit

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,16 +5,16 @@
 
 import express from 'express';
 import * as path from 'path';
-
+import cors from 'cors';
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to auth-service!' });
+  res.send({ message: 'Welcome to api-gateway!' });
 });
 
-const port = process.env.PORT || 6001;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
